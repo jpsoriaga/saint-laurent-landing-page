7f667408b062e2f442288b2c94cf0401175fa700
John Patrick Soriaga <patricksoriaga14@gmail.com>
2026-05-11 09:21:17 +0800

    add start animation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,29 @@
-import LandingPage from "./pages/LandingPage"
+import LandingPage from "./pages/LandingPage";
+import { motion } from "framer-motion";
 
 function App() {
-
   return (
     <>
       <LandingPage />
+
+      <motion.div
+        initial={{
+          opacity: 1,
+          scale: 1,
+          transformOrigin: "bottom right",
+        }}
+        animate={{
+          opacity: 0,
+          scale: 1.8,
+        }}
+        transition={{
+          duration: 0.9,
+          ease: [0.76, 0, 0.24, 1],
+        }}
+        className="fixed inset-0 bg-[#f5f1ed] z-[9999] pointer-events-none"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -5,7 +5,7 @@ export default function NavBar() {
         <>
             <div className=" flex items-center justify-between py-12 px-8">
                 <button className="button-primary group">
-                    <div className="flex flex-col transition-transform duration-300 group-hover:-translate-y-1/2">
+                    <div className="flex flex-col transition-transform duration-400 group-hover:-translate-y-1/2">
                         <span className="h-[40px]">MY PROFILE</span>
                         <span className="font-primary text-xl tracking-wider h-[40px]">MY PROFILE</span>
                     </div>
@@ -14,7 +14,7 @@ export default function NavBar() {
                 <img src={YslText} alt="YSL Logo Font" className="w-40 h-auto" />
 
                 <button className="button-primary group">
-                    <div className="flex flex-col transition-transform duration-300 group-hover:-translate-y-1/2">
+                    <div className="flex flex-col transition-transform duration-400 group-hover:-translate-y-1/2">
                         <span className="h-[40px]">LA MAISON</span>
                         <span className="font-primary text-xl tracking-wider h-[40px]">LA MAISON</span>
                     </div>
