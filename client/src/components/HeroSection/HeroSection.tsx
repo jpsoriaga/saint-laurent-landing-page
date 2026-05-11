0d77a540ddd2985bfd27710f3d4e52a71a09f19c
John Patrick Soriaga <patricksoriaga14@gmail.com>
2026-05-11 21:48:42 +0800

    update

--- client/src/components/HeroSection/HeroSection.tsx
+++ client/src/components/HeroSection/HeroSection.tsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
     return (
         <div className="relative pl-25 flex">
 
-            <div className="font-primary font-extralight text-[16rem] tracking-[1.3rem] leading-[0.85] z-10">
+            <div className="text-hero-section z-10">
                 <h1>YVES</h1>
 
                 <div className="flex justify-between">
@@ -19,15 +19,14 @@ export default function HeroSection() {
                 <h1>LAURENT</h1>
             </div>
 
-            <div className="absolute right-0 top-0 w-160 h-full overflow-hidden">
+            <div className="absolute right-0 top-0 w-[clamp(420px,40vw,850px)] h-full overflow-hidden">
                 <img
                     src={Model}
                     alt="Model Picture"
                     className="w-full h-full object-cover z-0 absolute"
                 />
 
-                <div className='absolute text-white -left-193 bottom-0 font-primary font-extralight 
-                text-[16rem] tracking-[1.3rem] leading-[0.85] z-20 text-right'>
+                <div className='absolute text-[#f1efee] text-hero-section  translate-x-[-76.5%] bottom-0 z-20 text-right'>
                     <h1>SAINT</h1>
                     <h1>LAURENT</h1>
                 </div>
--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -3,20 +3,20 @@ import YslText from "../../assets/ysl-text.png"
 export default function NavBar() {
     return (
         <>
-            <div className=" flex items-center justify-between py-12 px-8">
-                <button className="button-primary group">
+            <div className=" flex items-center justify-between py-12 px-5 sm:px-8">
+                <button className="button-primary group flex items-center">
                     <div className="flex flex-col transition-transform duration-400 group-hover:-translate-y-1/2">
-                        <span className="h-[40px]">MY PROFILE</span>
-                        <span className="font-primary text-xl tracking-wider h-[40px]">MY PROFILE</span>
+                        <span className="h-[40px] flex items-center">MY PROFILE</span>
+                        <span className="font-primary text-xl tracking-wider h-[40px] flex items-center">MY PROFILE</span>
                     </div>
                 </button>
 
-                <img src={YslText} alt="YSL Logo Font" className="w-40 h-auto" />
+                <img src={YslText} alt="YSL Logo Font" className="w-[clamp(120px,10vw,160px)] h-auto" />
 
-                <button className="button-primary group">
+                <button className="button-primary group flex items-center">
                     <div className="flex flex-col transition-transform duration-400 group-hover:-translate-y-1/2">
-                        <span className="h-[40px]">LA MAISON</span>
-                        <span className="font-primary text-xl tracking-wider h-[40px]">LA MAISON</span>
+                        <span className="h-[40px] flex items-center">LA MAISON</span>
+                        <span className="font-primary text-xl tracking-wider h-[40px] flex items-center">LA MAISON</span>
                     </div>
                 </button>
             </div>
