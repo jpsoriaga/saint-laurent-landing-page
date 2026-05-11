52b9577d09bde7fe8e7d0882a090422b8ba84325
John Patrick Soriaga <patricksoriaga14@gmail.com>
2026-05-11 22:00:19 +0800

    updated navbar

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -4,8 +4,8 @@ export default function NavBar() {
     return (
         <>
             <div className=" flex items-center justify-between py-12 px-5 sm:px-8">
-                <button className="button-primary group flex items-center">
-                    <div className="flex flex-col transition-transform duration-400 group-hover:-translate-y-1/2">
+                <button className="button-primary group">
+                    <div className="flex flex-col items-center transition-transform duration-400 group-hover:-translate-y-1/2">
                         <span className="h-[40px] flex items-center">MY PROFILE</span>
                         <span className="font-primary text-xl tracking-wider h-[40px] flex items-center">MY PROFILE</span>
                     </div>
@@ -13,7 +13,7 @@ export default function NavBar() {
 
                 <img src={YslText} alt="YSL Logo Font" className="w-[clamp(120px,10vw,160px)] h-auto" />
 
-                <button className="button-primary group flex items-center">
+                <button className="button-primary group">
                     <div className="flex flex-col transition-transform duration-400 group-hover:-translate-y-1/2">
                         <span className="h-[40px] flex items-center">LA MAISON</span>
                         <span className="font-primary text-xl tracking-wider h-[40px] flex items-center">LA MAISON</span>
